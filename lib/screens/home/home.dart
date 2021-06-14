346e81b1730f3bf305f7a0a93ee3fe1bbd987319
sandunsameera <sandunsameera25@gmail.com>
2021-06-14 19:37:46 +0530

    feat: add map view with polylines

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:train_alert/screens/home/find_train_screen.dart';
 import 'package:train_alert/services/authService.dart';
 
-class Home extends StatelessWidget{
+class Home extends StatelessWidget {
   final AuthService _authService = AuthService();
   @override
   Widget build(BuildContext context) {
@@ -11,8 +12,14 @@ class Home extends StatelessWidget{
         title: Text("Train Alert"),
         actions: [
           TextButton.icon(
-            icon: Icon(Icons.logout, color: Colors.white,),
-            label: Text("Logout",style: TextStyle(color: Colors.white),),
+            icon: Icon(
+              Icons.logout,
+              color: Colors.white,
+            ),
+            label: Text(
+              "Logout",
+              style: TextStyle(color: Colors.white),
+            ),
             onPressed: () async {
               await _authService.signOut();
             },
@@ -23,63 +30,65 @@ class Home extends StatelessWidget{
         child: Container(
           child: Column(
             children: [
-          Container(
-            // width: 90,
-            height: 250,
-            decoration: BoxDecoration(
-                // shape: BoxShape.circle,
-                image: DecorationImage(
-                    image: AssetImage('assets/images/station.jpg'),
-                    fit: BoxFit.fill
-                )
-            )
-        ),
+              Container(
+                  // width: 90,
+                  height: 250,
+                  decoration: BoxDecoration(
+                      // shape: BoxShape.circle,
+                      image: DecorationImage(
+                          image: AssetImage('assets/images/station.jpg'),
+                          fit: BoxFit.fill))),
               Row(
                 children: [
                   Expanded(
-                    child: GestureDetector(
-                      onTap: () => print("hello"),
-                      child: CustomCard(
-                        title: "Find train",
-                        subTitle: 'Find best train nearby',
-                        image: 'assets/images/findTrain.jpg',
-                        direction: "left",),
-                    )
-                  ),
-                  Expanded(
-                    child: GestureDetector(
-                      onTap: () => print("hello"),
-                      child: CustomCard(
-                        title: "View Trains",
-                        subTitle: 'View trains and their status',
-                        image: 'assets/images/viewTrain.jpg',
-                        direction: "right",),
-                    )
+                      child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          (MaterialPageRoute(
+                              builder: (context) => FindTrainScreen())));
+                    },
+                    child: CustomCard(
+                      title: "Find train",
+                      subTitle: 'Find best train nearby',
+                      image: 'assets/images/findTrain.jpg',
+                      direction: "left",
                     ),
+                  )),
+                  Expanded(
+                      child: GestureDetector(
+                    onTap: () {},
+                    child: CustomCard(
+                      title: "View Trains",
+                      subTitle: 'View trains and their status',
+                      image: 'assets/images/viewTrain.jpg',
+                      direction: "right",
+                    ),
+                  )),
                 ],
               ),
               Row(
                 children: [
                   Expanded(
-                    child: GestureDetector(
-                      onTap: () => print("hello"),
-                      child: CustomCard(
-                        title: "Train Schedule",
-                        subTitle: 'Find Trains by time and date',
-                        image: 'assets/images/schedule.jpg',
-                        direction: "left",),
-                    )
-                  ),
+                      child: GestureDetector(
+                    onTap: () => print("hello"),
+                    child: CustomCard(
+                      title: "Train Schedule",
+                      subTitle: 'Find Trains by time and date',
+                      image: 'assets/images/schedule.jpg',
+                      direction: "left",
+                    ),
+                  )),
                   Expanded(
-                    child: GestureDetector(
-                      onTap: () => print("hello"),
-                      child: CustomCard(
-                        title: "Profile",
-                        subTitle: 'View/Update profile',
-                        image: 'assets/images/profile.jpg',
-                        direction: "right",),
-                    )
-                  ),
+                      child: GestureDetector(
+                    onTap: () => print("hello"),
+                    child: CustomCard(
+                      title: "Profile",
+                      subTitle: 'View/Update profile',
+                      image: 'assets/images/profile.jpg',
+                      direction: "right",
+                    ),
+                  )),
                 ],
               )
             ],
@@ -96,37 +105,54 @@ class CustomCard extends StatelessWidget {
   final String direction;
   final String image;
 
-  CustomCard({@required this.title, @required this.subTitle, @required this.direction, @required this.image}) : assert(title != null && direction != null && subTitle != null && image != null);
+  CustomCard(
+      {@required this.title,
+      @required this.subTitle,
+      @required this.direction,
+      @required this.image})
+      : assert(title != null &&
+            direction != null &&
+            subTitle != null &&
+            image != null);
 
   @override
   Widget build(final BuildContext context) {
     return Container(
-      margin: (direction=='left') ? EdgeInsets.only(bottom:10.0 ,left: 40.0,right: 10.0,top: 10.0) : EdgeInsets.only(bottom:10.0 ,left: 10.0,right: 40.0,top: 10.0),
+      margin: (direction == 'left')
+          ? EdgeInsets.only(bottom: 10.0, left: 40.0, right: 10.0, top: 10.0)
+          : EdgeInsets.only(bottom: 10.0, left: 10.0, right: 40.0, top: 10.0),
       height: 200.0,
       child: Card(
         child: Column(
           children: [
             Container(
-              margin: EdgeInsets.only(top: 15.0),
-              width: 90,
-              height: 90,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                image: DecorationImage(
-                image: AssetImage(image),
-                fit: BoxFit.fill
-                )
-              )
+                margin: EdgeInsets.only(top: 15.0),
+                width: 90,
+                height: 90,
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                        image: AssetImage(image), fit: BoxFit.fill))),
+            SizedBox(
+              height: 5.0,
             ),
-            SizedBox(height: 5.0,),
-            Text(title, style: TextStyle(fontWeight: FontWeight.bold),),
+            Text(
+              title,
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
             Divider(
               height: 3.0,
               thickness: 2.0,
             ),
             Container(
               margin: EdgeInsets.only(left: 2.0, right: 2.0, top: 2.0),
-              child: Text(subTitle, style: TextStyle(color: Colors.grey,), textAlign: TextAlign.center,),
+              child: Text(
+                subTitle,
+                style: TextStyle(
+                  color: Colors.grey,
+                ),
+                textAlign: TextAlign.center,
+              ),
             )
           ],
         ),
@@ -134,4 +160,3 @@ class CustomCard extends StatelessWidget {
     );
   }
 }
-
